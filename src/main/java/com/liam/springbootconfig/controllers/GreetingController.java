d52922988f0aa56310e1353b69734e7710950c38
Liam Desmond <liamjdesmond84@gmail.com>
2022-02-28 15:22:50 -0600

    Adding Configs

--- src/main/java/com/liam/springbootconfig/controllers/GreetingController.java
+++ src/main/java/com/liam/springbootconfig/controllers/GreetingController.java
@@ -1,5 +1,8 @@
 package com.liam.springbootconfig.controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,15 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 	
-	@Value("${my.greeting}")
+	// String
+	@Value("${my.greeting: default value here}")
 	private String greetingMessage;
 	
-	@Value("some static message")
-	private String staticMessage;
+	// List
+	@Value("${my.list.values}")
+	private List<String> listValues;
+	
+	// Object(key:value) - hash(#) treats everything inside curly braces after it as a Spring expression language
+	@Value("#{${dbValues}}")
+	private Map<String, String> dbValues;
+	
+
 	
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage;
+		return greetingMessage + listValues + dbValues;
+		// Hey Suckas![One, Two, Three]{connectionString=http://__, userName=Karl, password=pass}
 	}
 
 }
