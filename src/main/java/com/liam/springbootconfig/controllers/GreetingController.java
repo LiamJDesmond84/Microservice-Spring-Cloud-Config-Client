cdd3f429ea64b9e39c74df10c10c39b1e01eb288
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-01 18:25:57 -0600

    Updating application file

--- src/main/java/com/liam/springbootconfig/controllers/GreetingController.java
+++ src/main/java/com/liam/springbootconfig/controllers/GreetingController.java
@@ -5,8 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.env.Environment;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +17,8 @@ public class GreetingController {
 	@Autowired
 	private DbSettings dbSettings;
 	
-	@Autowired
-	private Environment env;
+//	@Autowired
+//	private Environment env;
 	
 	// String
 	@Value("${my.greeting: default value here}")
