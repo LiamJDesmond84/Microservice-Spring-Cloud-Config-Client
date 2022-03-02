b0c87359757e8b36f37e6d441375a9e53142d1b1
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-01 20:04:30 -0600

    Updating application file

--- src/main/java/com/liam/springbootconfig/controllers/GreetingController.java
+++ src/main/java/com/liam/springbootconfig/controllers/GreetingController.java
@@ -5,13 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.liam.springbootconfig.models.DbSettings;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 	
 	@Autowired
